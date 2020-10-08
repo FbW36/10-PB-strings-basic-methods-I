0d63e0394431d734cf31c2f8a1ae66871d9c86c2
Joyce <puch1996@gmail.com>
2020-10-08 14:27:00 +0200

    modified

---
+++ Joyce/index.js
@@ -0,0 +1,33 @@
+//1
+let walk = "I can walk in the park all day!";
+console.log(walk.slice(18, 22));
+
+// 2
+let value = "JavaScript";
+console.log(value.slice(3, 6));
+
+// 3
+let sentence = "nice schoes";
+console.log(
+  'the sentence "nice shoes" contains the letter l ==> ',
+  sentence.includes("l")
+);
+console.log(
+  'the sentence "nice shoes" contains the letter n ==>',
+  sentence.includes("n")
+);
+
+// 4
+let firstName = "Joyce";
+let city = "Düsseldorf";
+let hobby = "cooking";
+console.log(
+  "My name is " +
+    firstName +
+    ". " +
+    "I live in " +
+    city +
+    " and I like " +
+    hobby +
+    "."
+);
