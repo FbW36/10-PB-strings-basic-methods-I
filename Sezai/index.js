cd4ccd3f29fdbed23a15e983a89650bd78e8271a
Sezai Keskin <sezai.keskin@me.com>
2020-10-08 14:11:48 +0200

    please review ex1 from today

---
+++ Sezai/index.js
@@ -0,0 +1,19 @@
+// 1.
+let freeTime = "I can walk in the park all day!"
+console.log("freeTime ==> ", freeTime.slice(18, 23));
+
+// 2. 
+let lang = "JavaScript"
+console.log("language ==> ", lang.slice(3, 6));
+
+//3. 
+let shoe = "nice shoes";
+console.log("nice shoes ==>", shoe.indexOf("l"));
+console.log("nice shoes ==>", shoe.indexOf("n"));
+
+//4. 
+let name = "Sezai";
+let city = "MG";
+let job = "WebDev"
+let person = `My name is ${name} and i am from ${city}. I am a ${job}.`
+console.log("person ==>", person)
