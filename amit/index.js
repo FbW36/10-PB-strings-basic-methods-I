56bc005765c600b9fa84af7b0a1162a36c1c11e2
Amit833 <amitmajumder833@gmail.com>
2020-10-08 15:07:32 +0200

    amit

---
+++ amit/index.js
@@ -0,0 +1,17 @@
+let fun = "I can walk in the park all day!";
+console.log("fun ==>", fun.slice(18, 22)); // park
+
+// 2
+let pro = "JavaScript";
+console.log("pro ==>", pro.slice(3, 6)); //aSc
+
+// 3
+let dress = "nice shoes";
+console.log("Dress ==>", dress.includes("n") || dress.includes("l")); // true
+
+// 4
+let name = "Amit Majumder";
+let city = "Berlin";
+let job = "teacher";
+let ans = `My name is ${name}. I live in ${city} and I am a ${job}.`;
+console.log(ans);
