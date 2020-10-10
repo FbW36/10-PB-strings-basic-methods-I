db3c6de26681f6950549275de4ba20d50ff15b1c
ciamac-da <ciamac.davoudi@gmail.com>
2020-10-10 17:53:23 +0200

    !

---
+++ Muhanad/hallo.js
@@ -0,0 +1,24 @@
+//1
+let text = "I can walk in the park all day!";
+console.log(walk.slice(18, 22));
+
+// 2
+let str = "JavaScript";
+console.log(value.slice(3, 6));
+
+// 3
+let look = "nice schoes";
+console.log(
+  'the sentence "nice shoes" contains the letter l ==> ',
+  sentence.includes("l")
+);
+console.log(
+  'the sentence "nice shoes" contains the letter n ==>',
+  sentence.includes("n")
+);
+
+// 4
+let firstName = "muhanad";
+let land = "Germany";
+let hobby = "swemming";
+console.log(`My name is ${firstName}. I live in ${land} and I like ${hobby}.`);
