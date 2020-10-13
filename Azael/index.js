874271d8e9a3b6e41eb14807a9170cbd46c1a8f0
Zarzarius <zarzarius@gmail.com>
2020-10-13 15:42:25 +0200

    initial commit

---
+++ Azael/index.js
@@ -0,0 +1,7 @@
+// 1. Declare a variable with the value of "I can walk in the park all day!". Print the word "park" to the console.
+
+// 2. Declare a variable with the value "JavaScript". Use string methods to print the the characters "aSc" from your declared variable.
+
+// 3. Check if the sentence "nice shoes" contains the letter l or n.
+
+// 4. Create 3 different variables about yourself, using strings as values, i.e, let firstName = "Maria"; let city = "Berlin", etc. Print a sentence to the console using the variables and string interpolation, i.e., "My name is Maria. I live in Berlin and I am a teacher".
